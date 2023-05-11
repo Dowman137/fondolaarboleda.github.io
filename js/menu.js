0c562b3cec88d3180613c5c71477b925eb4bc986
dominith <51300092+Dowman137@users.noreply.github.com>
2023-05-10 20:06:04 -0500

    primer commit

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+function mostrarHeader() {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").classList.add("show");
+      } else {
+        document.getElementById("header").classList.remove("show");
+      }
+      prevScrollpos = currentScrollPos;
+    }
+  }
+  
+  window.onload = function() {
+    mostrarHeader();
+    document.getElementById("header").classList.add("show");
+  };
+  
